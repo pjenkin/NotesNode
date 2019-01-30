45125ebcc21ef2b98c43cc3661cfda74d183c780
Peter N Jenkin <peternjenkin@gmail.com>
2019-01-30 13:49:17 +0000

    command line arguments used in 1 exported function

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');    // 3rd party (npm) module
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 // ./ relative path
 
+const argv = yargs.argv;    // use yargs
 var command = process.argv[2];
 var command2 = process.argv[3];
 
 console.log(process.argv);
 
 console.log('Command:', command);
+console.log('Process:', process.argv);
+console.log('Yargs:', argv);
 
 if (command === 'add')
 {
-  console.log('Adding new note');
+  // console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 }
 else if (command === 'list')
 {
--- notes.js
+++ notes.js
@@ -1,9 +1,17 @@
 console.log('starting notes.js');
 
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  var noteAdded = 'Adding Note';
-  return noteAdded;
+module.exports = {
+  //addNote: addNote    // the function defined above
+  addNote    // the function defined above (shorter syntax for function names in ES6)
 }
+
+// module.exports.addNote = () => {
+//   console.log('addNote');
+//   var noteAdded = 'Adding Note';
+//   return noteAdded;
+// }
 // NB arrow/lambda function notation =>
