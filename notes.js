cc2cb95b064aaab41394f2cd02213af75827321b
Peter N Jenkin <peternjenkin@gmail.com>
2019-01-30 12:16:58 +0000

    Tidy up notes.js

--- notes.js
+++ notes.js
@@ -1,15 +1,5 @@
 console.log('starting notes.js');
 
-// console.log(module);
-
-module.exports.age=45;
-
-module.exports.logtest = 'exported starting notes.js';
-// NB module.exports.whatever
-
-// module.exports.addNote = function () {
-//
-// }
 
 module.exports.addNote = () => {
   console.log('addNote');
@@ -17,8 +7,3 @@ module.exports.addNote = () => {
   return noteAdded;
 }
 // NB arrow/lambda function notation =>
-
-
-module.exports.add = (a,b) => {
-  return a + b;
-}
