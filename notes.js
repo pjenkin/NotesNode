04a0762d46abfc2eec2088726e5d047265ae6c66
Peter N Jenkin <peternjenkin@gmail.com>
2019-01-30 18:40:32 +0000

    fs.writeFileSync and lambda expression for writing unique notes

--- notes.js
+++ notes.js
@@ -1,7 +1,39 @@
 console.log('starting notes.js');
 
+const fs = require('fs');   // core node module
+
 var addNote = (title, body) => {
   console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body    // ES6 no need for body:body
+  };
+
+// try/catch in case file not yet existing
+try {
+  var notesString = fs.readFileSync('notes-data.json');   // get existing notes
+  notes = JSON.parse(notesString);
+} catch (e) {
+
+}
+
+// look for notes with this title already
+// var duplicateNotes = notes.filter((note) => {
+//   return note.title === title;
+// });
+// short-hand of above, with E6 lambda/arrow
+var duplicateNotes = notes.filter((note) =>  note.title === title);
+// console.log(duplicateNotes.length);
+// console.log(duplicateNotes[0].title);
+// console.log(duplicateNotes[1].title);
+// go ahead if no duplicates (none with this title already), write new note, else don't save
+if (duplicateNotes.length === 0) {
+  notes.push(note);
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
+
 };
 
 var getAll = () => {
