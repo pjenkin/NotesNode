52926f37089b92d42bec7e188fbb3c752384f379
Peter N Jenkin <peternjenkin@gmail.com>
2019-01-30 14:34:58 +0000

    JSON parsing and file I/O as per challenge 3-15

--- playground/json.js
+++ playground/json.js
@@ -1,14 +1,33 @@
-var obj = {
-  name: 'PNJ'
+// var obj = {
+//   name: 'PNJ'
+// };
+// var stringObj = JSON.stringify(obj);
+//
+// console.log('typeof stringObj: ',typeof stringObj);
+// console.log('stringObj: ',stringObj);
+//
+//
+// var personString = '{"name":"Peter","age":45}';    // NB double quotes in JSON
+// var person = JSON.parse(personString);
+//
+// console.log('typeof person: ',typeof person);
+// console.log('person: ',person);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
 };
-var stringObj = JSON.stringify(obj);
 
-console.log('typeof stringObj: ',typeof stringObj);
-console.log('stringObj: ',stringObj);
+// var originalNoteString = JSON.stringify({"title":"First","body":"The first note"});
+var originalNoteString = JSON.stringify(originalNote);
 
+fs.writeFileSync('notes.json', originalNoteString);
 
-var personString = '{"name":"Peter","age":45}';    // NB double quotes in JSON
-var person = JSON.parse(personString);
+var noteString = fs.readFileSync('notes.json');
 
-console.log('typeof person: ',typeof person);
-console.log('person: ',person);
+var note = JSON.parse(noteString);
+
+console.log('typeof note: ',typeof note);
+console.log('note: ',note);
