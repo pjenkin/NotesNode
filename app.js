b2318a80cc9b7d509da67d3a14422462d50bbbab
Peter N Jenkin <peternjenkin@gmail.com>
2019-01-30 12:36:03 +0000

    argv used for command line arguments

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ const _ = require('lodash');    // 3rd party (npm) module
 
 const notes = require('./notes.js');
 // ./ relative path
+
+var command = process.argv[2];
+var command2 = process.argv[3];
+
+console.log(process.argv);
+
+console.log('Command:', command);
+
+if (command === 'add')
+{
+  console.log('Adding new note');
+}
+else if (command === 'list')
+{
+  console.log('Listing all notes');
+} else {
+  console.log('Command not recognised');
+}
